7d49a9cf1ab5cef345bea688b690d680a27a6323
zhoutanxin <zhoutanxin@hotmail.com>
2015-11-17 19:43:01 +0800

    首页内容拆分

--- js/script.js
+++ js/script.js
@@ -1,9 +1,14 @@
-
-  // niceScroll
+function loadPage(url){
+    $("#iframe").load(url);
+}
+loadPage("content.html");
+$("#item-download").click(function(){
+    loadPage('apps.html');
+});
+  // niceScroll滚动条
   $("html").niceScroll();
-    
-    
-  // Stick menu
+
+  // Stick menu头部菜单
   $(".menu").sticky({topSpacing:0});
   $("#pop-menu").sticky({topSpacing:0});
 
@@ -16,20 +21,18 @@
       var item = $($(this).attr("href"));
       if (item.length) { return item; }
     });
-
+    var target;
    $('a[href*=#]').bind('click', function(e) {
-	e.preventDefault();
-       
-	var target = $(this).attr("href");
-       alert($("page1").html());
-       $("#iframe").html($("page1").html());
-       $('html, body').stop().animate({ scrollTop: $(target).offset().top-140 }, 1000, function() {
-
-	});
-			
-	return false;
+        e.preventDefault();
+        target = $(this).attr("href");
+           loadPage("content.html");
+           $('html, body').stop().animate({ scrollTop: $(target).offset().top-140 }, 1000, function() {
+           });
+        return false;
    });
-
+if(target){
+    $(target).trigger("click");
+}
   $(window).scroll(function(){
    var fromTop = $(this).scrollTop()+topMenuHeight;
    var cur = scrollItems.map(function(){
@@ -45,39 +48,31 @@
        menuItems
          .parent().removeClass("active")
          .end().filter("[href=#"+id+"]").parent().addClass("active");
-   }                   
-  });  
+   }
+      console.log("windowTop"+$(this).offset().top+"fromTop:"+fromTop+" cur:"+cur+" id:"+id+" lastId:"+lastId+" curLength:"+cur.length);
+ });
   
 
 if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
-    
     $(".footer").css( "position", "relative" );
     $(".contact").css( "marginBottom", "0" );
-
-}
-else 
-{
-
+}else {
   // FadeTo elements
-  if ( $(window).width() > 1023) {  
-
-    tiles = $("h2, h3, .column-one, .column-two, .column-three, .grid li, .contact .content .form, .contact .content .contact-text ").fadeTo(0, 0);
-
-    $(window).scroll(function(d,h) {
-      tiles.each(function(i) {
-          a = $(this).offset().top + $(this).height();
-          b = $(window).scrollTop() + $(window).height();
-          if (a < b) $(this).fadeTo(1000,1);
-      });
-    });
-
-  }
+    if ( $(window).width() > 1023) {
+            tiles = $("h2, h3, .column-one, .column-two, .column-three, .grid li, .contact .content .form, .contact .content .contact-text ").fadeTo(0, 0);
+            $(window).scroll(function(d,h) {
+              tiles.each(function(i) {
+                  a = $(this).offset().top + $(this).height();
+                  b = $(window).scrollTop() + $(window).height();
+                  if (a < b) $(this).fadeTo(1000,1);
+              });
+            });
+      }
 
 }
 
 
-
-  //Menu mobile click
+  //Menu mobile click 移动版菜单点击事件
   $( ".icon" ).click(function() {
     $( " ul.menu-mobile" ).slideToggle( "slow", function() {
     // Animation complete.
@@ -90,7 +85,6 @@ else
     });
   });
   $("#m-item-about").click(function() {
-
     $( ".menu-mobile-about" ).slideToggle( "slow", function() {
     });
   });
@@ -98,16 +92,14 @@ else
 
 $(window).load(function(){
 
-$(".preloader").delay(1000).fadeOut("slow")
-
-  // Parallax
-  if ($('.parallax-background').length) {
-    $(".parallax-background").parallax();
-  }
-  
-  // Parallax
-  if ($('.parallax-background-partners').length) {
-    $(".parallax-background-partners").parallax();
-  }  
+    $(".preloader").delay(1000).fadeOut("slow")
+      // Parallax
+      if ($('.parallax-background').length) {
+        $(".parallax-background").parallax();
+      }
+      // Parallax
+      if ($('.parallax-background-partners').length) {
+        $(".parallax-background-partners").parallax();
+      }
 
 });
